3559b00c54d4b015522d24829af3c38fc329c80a
yumii4548 <qzhi415@gmail.com>
2026-08-03 22:19:50 +0800

    MewBaka图片转WebP

--- common.js
+++ common.js
@@ -114,7 +114,7 @@
         { name: '云筑设计团队', en: 'Cloud Pavilion Work', type: '建筑团队', desc: '云筑设计团队由HOYSH创立，成立于2026年3月12日，主营现代式建筑，代表作：《天宗市》《希伦市》《像素大同系列》《像素深圳》', url: 'https://v.douyin.com/8_n7XMWClPc/', color: '#8b5cf6', logo: 'fa-solid fa-microchip', image: 'images/yzlogo.webp' },
         { name: '鸽予文化', en: 'Ge Yu Culture', type: '合作组织', desc: '上海鸽予文化有限公司，成立于2024年12月，主业为新媒体宣发及文化设计，与多名创作者展开合作，旗下鸽予工作室主营游戏《我的世界》相关物料制作。', url: '#', color: '#10b981', logo: 'fa-solid fa-cubes', image: 'images/gy.webp' },
         { name: '宇创领航工作室', en: 'YuChuang Studio', type: '合作组织', desc: '宇创领航是《我的世界》中国版综合开发工作室，深耕网易版生态，主营模组、光影、城市还原地图创作。擅长轨道交通、都市建造模组，代表作有像素邯郸、轨道交通模组、校园组件，还自研多种应用和游戏，兼顾创作与新人培育，全网传播热度较高。', url: 'https://vhwork.cn/', color: '#94a3b8', logo: 'fa-solid fa-handshake', image: 'images/yclh.webp' },
-        { name: 'MewBaka笨猫工作室', en: 'MewBaka Studio', type: '合作组织', desc: '笨猫工作室成立于2024年7月，主要开发视觉小说游戏及衍生产品，正在制作一款专为视觉小说创作打造的现代游戏引擎——NarraLeaf Project。', url: '#', color: '#f97316', logo: 'fa-solid fa-cat', image: '' }
+        { name: 'MewBaka笨猫工作室', en: 'MewBaka Studio', type: '合作组织', desc: '笨猫工作室成立于2024年7月，主要开发视觉小说游戏及衍生产品，正在制作一款专为视觉小说创作打造的现代游戏引擎——NarraLeaf Project。', url: '#', color: '#f97316', logo: 'fa-solid fa-cat', image: 'images/mewbaka.webp' }
     ];
 
     var FRIEND_LINKS = [
